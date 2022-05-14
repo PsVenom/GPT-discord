19fbe22540a23e8bb9046e1d96130230ddbff156
PsVenom <99831413+PsVenom@users.noreply.github.com>
2022-05-14 18:56:15 +0530

    Update sexting.py

--- sexting.py
+++ sexting.py
@@ -1,5 +1,5 @@
 import os
-
+from main import ask
 import discord
 from discord.utils import get
 from discord.ext import commands, tasks
@@ -8,33 +8,33 @@ TOKEN = 'OTcyODczNTA2Mzg5OTE3NzY3.YnfZDw.jkVsaoaRXU5zIkAo-buALowPs3I'
 
 client = discord.Client()
 
-bots = commands.Bot(command_prefix=['sext ','s!'], help_command=None)
+bots = commands.Bot(command_prefix=['sext ', 's!'], help_command=None)
+
 
 class help(commands.Cog):
-
-    bots = commands.Bot(command_prefix=['sext ','s!'], help_command=None)
+    bots = commands.Bot(command_prefix=['sext ', 's!'], help_command=None)
 
     def __init__(self, bot):
         self.bot = bot
-        
+
     @bots.command(name='help')
     async def help(self, ctx):
-
-        title=''
-        description=''
-        z=int('',16)
+        title = ''
+        description = ''
+        z = int('', 16)
 
         embed = discord.Embed(title=title, description=description, colour=discord.Colour(z))
 
+
 bots.add_cog(help(bots))
 
-class sext(commands.Cog):
 
-    bots = commands.Bot(command_prefix=['sext ','s!'], help_command=None)
+class sext(commands.Cog):
+    bots = commands.Bot(command_prefix=['sext ', 's!'], help_command=None)
 
     def __init__(self, bot):
         self.bot = bot
-        
+
     @bots.command(name='sext')
     async def sext(self, ctx):
 
@@ -47,15 +47,15 @@ class sext(commands.Cog):
             def check(m):
                 return m.author == ctx.author
 
-            msg = await bots.wait_for('message',check=check)
-            msg_cont=msg.content
+            msg = await bots.wait_for('message', check=check)
+            msg_cont = msg.content
 
-            if msg_cont=='end':
-                
+            if msg_cont == 'end':
                 await channel.send('bbye baby <3 UwU')
                 break
 
-            await channel.send('baby!!')
+            await channel.send(ask(msg_cont))
+
 
 bots.add_cog(sext(bots))
 
