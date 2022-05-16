c4067457d872405c0ee4e7ffe7769f0ad60e7be5
Parth Sharma <99831413+PsVenom@users.noreply.github.com>
2022-05-17 04:08:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
 
-from main import ask, fit
+from main import ask, fit, scraper
 import discord
 from discord.utils import get
 from discord.ext import commands, tasks
 import json
+global model
 
-
-TOKEN = ''
+TOKEN = 'OTcyODczNTA2Mzg5OTE3NzY3.GYgm4S.fqzm4JJSC6bpyYX8DGGu3HL34m4XqrltTuBmOA'
 
 client = discord.Client()
 
@@ -14,7 +14,7 @@ bots = commands.Bot(command_prefix=['sext ', 's!'], help_command=None)
 
 global msg_cont
 class help(commands.Cog):
-    bots = commands.Bot(command_prefix=['sext ', 's!'], help_command=None)
+    bots = commands.Bot(command_prefix=['sext ', 's'], help_command=None)
 
     def __init__(self, bot):
         self.bot = bot
@@ -37,13 +37,14 @@ class sext(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+
     @bots.command(name='sext')
     async def sext(self, ctx):
 
         await ctx.send('Ram ram cutie UwU <3')
 
         answer = 0
-
+        msg_cont1 = ""
         channel = ctx.message.channel
 
         while True:
@@ -67,25 +68,49 @@ class sext(commands.Cog):
                 break
 
             if answer != 0:
-                json_obj = json.dumps({"prompt": answer, "completion": msg_cont})
+                json_obj = json.dumps({"prompt": msg_cont1, "completion": msg_cont})
 
                 with open("train.json", "w") as outfile:
                     outfile.write(json_obj)
             try:
-                answer = ask(msg_cont)
+                answer = await ask(msg_cont, a)
             except discord.ext.commands.errors.CommandInvokeError as e:
                 answer = "YOUR MOTHER"
                 continue
-
+            ms_cont1 = msg_cont
             await channel.send(answer)
             try:
-                fit("scratch.json")
+                model = fit("train.json")
                 print("fit sucessful")
             except RuntimeError as e:
                 continue
 
 
+class AhegaoPic(commands.Cog):
+    bots = commands.Bot(command_prefix=['AhegaoPic', 'ap'], help_command=None)
+    def __init__(self, bot):
+        self.bot = bot
+
+    @bots.command(name='AhegaoPic')
+    async def record(self, ctx):
+         channel = ctx.message.channel
+         def check(m):
+            return m.author == ctx.author
+         a = scraper()
+         print(a)
+         embed = discord.Embed()
+         try:
+          img = embed.set_image(url= str(a))
+         except discord.ext.commands.errors.CommandInvokeError as e:
+          img = embed.set_image(url="https://qph.fs.quoracdn.net/main-qimg-965b11ec95106e64d37f5c380802c305-lq")
+         file = discord.File(fp= open("", "rb"), filename="image.png")
+         await ctx.send(file=file, embed=embed)
+
+
+
+
+
 
 bots.add_cog(sext(bots))
-
+bots.add_cog(AhegaoPic(bots))
 bots.run(TOKEN)
