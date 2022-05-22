ea7f86ae0ec90888bae33d5ff2bf1abb13d0ddda
Parth Sharma <99831413+PsVenom@users.noreply.github.com>
2022-05-23 00:34:02 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,19 +1,20 @@
 
-from main import ask, fit, scraper
+from main import ask, askCrack, scraper, getImg
 import discord
 from discord.utils import get
 from discord.ext import commands, tasks
 import json
+import random
 global model
-
-TOKEN = 'ENTER-BOT-TOKEN-HERE'
-
+import nextcord
+#discord api token
+TOKEN = 'OTcyODczNTA2Mzg5OTE3NzY3.GHNR-Z.oGM6m6dd4Pf_A38hiCnHeBJpwKYYLq0A6CmQ0g'
+#initialise api object
 client = discord.Client()
-
+#initialise bot
 bots = commands.Bot(command_prefix=['sext ', 's!'], help_command=None)
 
 global msg_cont
-
 class help(commands.Cog):
     bots = commands.Bot(command_prefix=['sext ', 's'], help_command=None)
 
@@ -27,23 +28,20 @@ class help(commands.Cog):
         z = int('', 16)
 
         embed = discord.Embed(title=title, description=description, colour=discord.Colour(z))
-
+#vanilla chat (activated by 'sext s')
 class sext(commands.Cog):
-    bots = commands.Bot(command_prefix=['sext ', 's!'], help_command=None)
+    bots = commands.Bot(command_prefix=['s ', 'sext'], help_command=None)
 
     def __init__(self, bot):
         self.bot = bot
 
 
-    @bots.command(name='sext')
-    async def sext(self, ctx):
-
-        await ctx.send('Ram ram cutie UwU <3')
-
+    @bots.command(name= 's')
+    async def s(self, ctx):
+        thread = await ctx.channel.create_thread(name = f'chat with {ctx.author} UwU',  message=None, auto_archive_duration=60, type= nextcord.ChannelType.public_thread, reason=None)
         answer = 0
         msg_cont1 = ""
-        channel = ctx.message.channel
-
+        await thread.send('sup hottie')
         while True:
 
             f = 0
@@ -52,7 +50,7 @@ class sext(commands.Cog):
                 return m.author == ctx.author
 
             try:
-                msg = await bots.wait_for('message', timeout=20.0, check=check)
+                msg = await bots.wait_for('message', timeout=120.0, check=check)
                 msg_cont = msg.content
             except:
                 f = 1
@@ -61,7 +59,7 @@ class sext(commands.Cog):
             b = f == 1
 
             if a or b:
-                await channel.send('bbye baby <3 UwU')
+                await thread.send('bbye baby <3 UwU')
                 break
 
             if answer != 0:
@@ -70,25 +68,68 @@ class sext(commands.Cog):
                 with open("train.json", "w") as outfile:
                     outfile.write(json_obj)
             try:
-                answer = await ask(msg_cont, a)
-            except discord.ext.commands.errors.CommandInvokeError as e:
+                answer = ask(msg_cont)
+            except:
                 answer = "YOUR MOTHER"
                 continue
             ms_cont1 = msg_cont
-            await channel.send(answer)
-            try:
-                model = fit("train.json")
-                print("fit sucessful")
-            except RuntimeError as e:
-                continue
+            await thread.send(answer)
+        await thread.delete()
+#cracked chat (activated by 'sext sc')
+class sextC(commands.Cog):
+    bots = commands.Bot(command_prefix=['sc ', 'sextC'], help_command=None)
 
-
-class AhegaoPic(commands.Cog):
-    bots = commands.Bot(command_prefix=['AhegaoPic', 'ap'], help_command=None)
     def __init__(self, bot):
         self.bot = bot
 
-    @bots.command(name='AhegaoPic')
+
+    @bots.command(name= 'sc')
+    async def sc(self, ctx):
+        thread = await ctx.channel.create_thread(name = f'chat with {ctx.author} UwU',  message=None, auto_archive_duration=60, type= nextcord.ChannelType.public_thread, reason=None)
+        answer = 0
+        msg_cont1 = ""
+        await thread.send('sup hottie')
+        while True:
+
+            f = 0
+
+            def check(m):
+                return m.author == ctx.author
+
+            try:
+                msg = await bots.wait_for('message', timeout=120.0, check=check)
+                msg_cont = msg.content
+            except:
+                f = 1
+
+            a = msg_cont == 'end'
+            b = f == 1
+
+            if a or b:
+                await thread.send('bbye baby <3 UwU')
+                break
+
+            if answer != 0:
+                json_obj = json.dumps({"prompt": msg_cont1, "completion": msg_cont})
+
+                with open("train.json", "w") as outfile:
+                    outfile.write(json_obj)
+            try:
+                answer = askCrack(msg_cont)
+            except:
+                answer = "YOUR MOTHER"
+                continue
+            ms_cont1 = msg_cont
+            await thread.send(answer)
+        await thread.delete()
+
+#ahegao fetching (activated by sext ap)
+class AhegaoPic(commands.Cog):
+    bots = commands.Bot(command_prefix=['ap', 'AhegaoPic'], help_command=None)
+    def __init__(self, bots):
+        self.bot = bots
+
+    @bots.command(name='ap')
     async def record(self, ctx):
          channel = ctx.message.channel
          def check(m):
@@ -96,20 +137,35 @@ class AhegaoPic(commands.Cog):
          a = scraper()
          print(a)
          embed = discord.Embed()
-         try:
-          img = embed.set_image(url= str(a))
-         except discord.ext.commands.errors.CommandInvokeError as e:
-          img = embed.set_image(url="https://qph.fs.quoracdn.net/main-qimg-965b11ec95106e64d37f5c380802c305-lq")
-         file = discord.File(fp= open("", "rb"), filename="image.png")
-         await ctx.send(file=file, embed=embed)
+         file = discord.File(fp = await getImg(), filename = 'Ahegao.jpg')
+         await ctx.reply(file = file)
+#self-explainatory (activated by sext sauce)
+class sauce(commands.Cog):
+    bots = commands.Bot(command_prefix=['sauce', 'sc'], help_command=None)
+    def __init__(self, bot):
+        self.bot = bot
+    @bots.command(name = 'sauce')
+    async def sauce(self, ctx):
+       file = open('codes.csv', 'r').read()
+       a = list(file.split(", "))
+       await ctx.send("https://nhentai.net/g/"+ a[random.randint(0, len(a)+1)])
+#additional porn command
+class p(commands.Cog):
+    bots = commands.Bot(command_prefix=['p', 'porn'], help_command=None)
 
+    def __init__(self, bot):
+        self.bot = bot
 
-
-
-
+    @bots.command(name='p')
+    async def p(self, ctx):
+        hyperlink = scraper(url_link = 'https://realbooru.com/index.php?page=post&s=list&tags=brazzers', remove = 5)
+        ctx.send(hyperlink)
 
 
 bots.add_cog(help(bots))
 bots.add_cog(sext(bots))
+bots.add_cog(sextC(bots))
 bots.add_cog(AhegaoPic(bots))
+bots.add_cog(sauce(bots))
+bots.add_cog(p(bots))
 bots.run(TOKEN)
--- main.py
+++ main.py
@@ -5,9 +5,11 @@ import os
 from bs4 import BeautifulSoup
 import urllib
 import cv2
-
-openai.api_key = 'ENTER-OPENAI-TOKEN-HERE'
+#adding api key
+openai.api_key = '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'
+#creating api instance
 completion = openai.Completion()
+# this is meant for the scraper (experimental)
 header= {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) ' 
       'AppleWebKit/537.11 (KHTML, like Gecko) '
       'Chrome/23.0.1271.64 Safari/537.11',
@@ -16,37 +18,36 @@ header= {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) '
       'Accept-Encoding': 'none',
       'Accept-Language': 'en-US,en;q=0.8',
       'Connection': 'keep-alive'}
-async def ask(question, model_exist = False, chat_log=None):
+#vanilla chatbot, shorter and more natural
+def ask(question, chat_log=None):
     if chat_log is None:
         chat_log = "Human: Hello, who are you? AI: I am an AI created by OpenAI. How can I help you today? "
     prompt = f'{chat_log}Human: {question}\nAI:'
-    if os.path.isdir("model.csv"):
-       with open("model.csv", "ab") as file:
-           a = pickle.load(file)
-       ft = openai.FineTune.retrieve(id= a["id"])
-       ftm = ft["fine_tuned_model"]
-
-       response = completion.create(model= ftm, prompt=prompt, engine="davinci", stop=['\nHuman'], temperature=0.9,
-        top_p=1, frequency_penalty=0, presence_penalty=0.6, best_of=1,
-        max_tokens=70)
-    else:
-     response = completion.create(prompt=prompt, engine="davinci", stop=['\nHuman'], temperature=0.9,
-        top_p=1, frequency_penalty=0, presence_penalty=0.6, best_of=1,
-        max_tokens=70)
-     model_exist = True
-    answer = response.choices[-1].text.strip()
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt="The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: My cock is throbbing, what do I do?\nAI: If you're experiencing arousal, you could masturbate or have sex with a consenting partner.\nHuman: I am pretty damn sad, what do I do?\n\nAI:\n\nThere are many things you can do if you're feeling sad. Some people might find comfort in talking to friends or family, while others might prefer to distraction themselves with activities they enjoy. Some other possible options include listening to music, reading, or spending time outdoors.\nHuman: ",
+        temperature=0.9,
+        max_tokens=150,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0.6,
+        stop=[" Human:", " AI:"]
+    )
+    answer = response.choices[0].text.strip()
     return answer
 
-
-def fit(filepath = 'scratch.json'):
-    with open(filepath, "r+") as f:
-        response = openai.File.create(file=f, purpose='fine-tune')
-    response = openai.FineTune.create(training_file=response["id"], model='davinci' )
-    file = open('model.csv', "wb")
-    pickle.dump(response, file)
-    file.close()
-
-def scraper(url_link = "https://www.google.com/search?q=ahegao&rlz=1C1YQLS_enIN994IN994&sxsrf=ALiCzsYUc788mZWD-tAhRGNmklg8-rrpAw:1652733172219&source=lnms&tbm=isch&sa=X&ved=2ahUKEwifzY3t7uT3AhXEjOYKHXroCx0Q_AUoAXoECAIQAw"):
+#crackhead version. more context focussed, but increadibly retarded
+def askCrack(question, chat_log=None):
+    if chat_log is None:
+        chat_log = "Human: Hello, who are you? AI: I am an AI created by OpenAI. How can I help you today? "
+    prompt = f'{chat_log}Human: {question}\nAI:'
+    response = completion.create(prompt=prompt, engine="davinci", stop=['\nHuman'], temperature=0.9,
+        top_p=1, frequency_penalty=0, presence_penalty=0.6, best_of=1,
+        max_tokens=70)
+    answer = response.choices[0].text.strip()
+    return answer
+#experimental, uses url scraping using beautifulsoup
+def scraper(url_link = "https://www.google.com/search?q=ahegao&rlz=1C1YQLS_enIN994IN994&sxsrf=ALiCzsYUc788mZWD-tAhRGNmklg8-rrpAw:1652733172219&source=lnms&tbm=isch&sa=X&ved=2ahUKEwifzY3t7uT3AhXEjOYKHXroCx0Q_AUoAXoECAIQAw", remove = 0):
  a = []
  request = urllib.request.Request(url_link, None, header)  # The assembled request
  response = urllib.request.urlopen(request)
@@ -55,10 +56,11 @@ def scraper(url_link = "https://www.google.com/search?q=ahegao&rlz=1C1YQLS_enIN9
  images = soup.find_all('img')
  for item in images:
      a.append(item['src'])
- return a[random.randint(0, len(a))]
+ return a[random.randint(remove, len(a))]
 
-
+#uses the ahegao dataset to get images
 async def getImg(fp = 'danbooru2020-ahegao-handpicked-cropped'):
     a = list(os.listdir(fp))
     return fp+"/"+a[random.randint(0, len(a))]
+
 
