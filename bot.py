8ffc91a940c3830ece1e0978ad238048f8f27727
Parth Sharma <99831413+PsVenom@users.noreply.github.com>
2022-07-12 23:26:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 
-from main import ask, askCrack, scraper, getImg
+from main import ask,  scraper, getImg
 import discord
 from discord.utils import get
 from discord.ext import commands, tasks
@@ -28,20 +28,19 @@ class help(commands.Cog):
         z = int('', 16)
 
         embed = discord.Embed(title=title, description=description, colour=discord.Colour(z))
-#vanilla chat (activated by 'sext s')
-class sext(commands.Cog):
-    bots = commands.Bot(command_prefix=['s ', 'sext'], help_command=None)
+class text(commands.Cog):
+    bots = commands.Bot(command_prefix=['t ', 'text'], help_command=None)
 
     def __init__(self, bot):
         self.bot = bot
 
 
-    @bots.command(name= 's')
-    async def s(self, ctx):
-        thread = await ctx.channel.create_thread(name = f'chat with {ctx.author} UwU',  message=None, auto_archive_duration=60, type= nextcord.ChannelType.public_thread, reason=None)
+    @bots.command(name= 't')
+    async def t(self, ctx):
+        thread = await ctx.channel.create_thread(name = f'chat with {ctx.author}',  message=None, auto_archive_duration=60, type= nextcord.ChannelType.public_thread, reason=None)
         answer = 0
         msg_cont1 = ""
-        await thread.send('sup hottie')
+        await thread.send('sup')
         while True:
 
             f = 0
@@ -59,7 +58,7 @@ class sext(commands.Cog):
             b = f == 1
 
             if a or b:
-                await thread.send('bbye baby <3 UwU')
+                await thread.send('bbye!')
                 break
 
             if answer != 0:
@@ -70,25 +69,28 @@ class sext(commands.Cog):
             try:
                 answer = ask(msg_cont)
             except:
-                answer = "YOUR MOTHER"
+                answer = "Exception occured- logging error"
                 continue
             ms_cont1 = msg_cont
             await thread.send(answer)
         await thread.delete()
-#cracked chat (activated by 'sext sc')
-class sextC(commands.Cog):
-    bots = commands.Bot(command_prefix=['sc ', 'sextC'], help_command=None)
-
+        
+class tts(commands.Cog):
+    bots = commands.Bot(command_prefix=['tts'], help_command=None)
     def __init__(self, bot):
         self.bot = bot
 
-
-    @bots.command(name= 'sc')
-    async def sc(self, ctx):
-        thread = await ctx.channel.create_thread(name = f'chat with {ctx.author} UwU',  message=None, auto_archive_duration=60, type= nextcord.ChannelType.public_thread, reason=None)
+    @bots.command(name = 'tts')
+    async def tts(self,ctx):
+        global vc
+        channel = ctx.message.channel
         answer = 0
         msg_cont1 = ""
-        await thread.send('sup hottie')
+        voicechannel = discord.utils.get(ctx.guild.channels, name='General')
+        try:
+         vc = await voicechannel.connect()
+        except:
+            vc
         while True:
 
             f = 0
@@ -106,66 +108,70 @@ class sextC(commands.Cog):
             b = f == 1
 
             if a or b:
-                await thread.send('bbye baby <3 UwU')
+                await channel.send('tts disconnedted due to inactivity')
                 break
 
-            if answer != 0:
-                json_obj = json.dumps({"prompt": msg_cont1, "completion": msg_cont})
-
-                with open("train.json", "w") as outfile:
-                    outfile.write(json_obj)
             try:
-                answer = askCrack(msg_cont)
+                output = gTTS(text=msg_cont, lang="en", tld="co.in")
+                output.save(f"tts.mp3")
+                vc.play(discord.FFmpegPCMAudio(source="tts.mp3", executable='C:/ffmpeg/bin/ffmpeg.exe'))
             except:
-                answer = "YOUR MOTHER"
+                answer = "Exception occured, recording in log"
+
+class vc(commands.Cog):
+    bots = commands.Bot(command_prefix=['vc '], help_command=None)
+
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    @bots.command(name= 'vc')
+    async def vc(self, ctx):
+        global vc
+        channel=ctx.message.channel
+        answer = 0
+        msg_cont1 = ""
+        voicechannel = discord.utils.get(ctx.guild.channels, name='General')
+        try:
+            vc = await voicechannel.connect()
+        except:
+            vc
+        output = gTTS(text='sup', lang="en", tld="co.in")
+        output.save(f"tts.mp3")
+        vc.play(discord.FFmpegPCMAudio(source="tts.mp3", executable='C:/ffmpeg/bin/ffmpeg.exe'))
+        while True:
+
+            f = 0
+
+            def check(m):
+                return m.author == ctx.author
+
+            try:
+                msg = await bots.wait_for('message', timeout=120.0, check=check)
+                msg_cont = msg.content
+            except:
+                f = 1
+
+            a = msg_cont == 'end'
+            b = f == 1
+
+            if a or b:
+                await channel.send('bbye baby <3 UwU')
+                break
+
+            try:
+                answer = ask(msg_cont)
+                output = gTTS(text=answer, lang="en", tld="co.in")
+                output.save(f"tts.mp3")
+                vc.play(discord.FFmpegPCMAudio(source="tts.mp3", executable='C:/ffmpeg/bin/ffmpeg.exe'))
+            except:
+                answer = "Exception occured, logging error"
                 continue
             ms_cont1 = msg_cont
-            await thread.send(answer)
-        await thread.delete()
-
-#ahegao fetching (activated by sext ap)
-class AhegaoPic(commands.Cog):
-    bots = commands.Bot(command_prefix=['ap', 'AhegaoPic'], help_command=None)
-    def __init__(self, bots):
-        self.bot = bots
-
-    @bots.command(name='ap')
-    async def record(self, ctx):
-         channel = ctx.message.channel
-         def check(m):
-            return m.author == ctx.author
-         a = scraper()
-         print(a)
-         embed = discord.Embed()
-         file = discord.File(fp = await getImg(), filename = 'Ahegao.jpg')
-         await ctx.reply(file = file)
-#self-explainatory (activated by sext sauce)
-class sauce(commands.Cog):
-    bots = commands.Bot(command_prefix=['sauce', 'sc'], help_command=None)
-    def __init__(self, bot):
-        self.bot = bot
-    @bots.command(name = 'sauce')
-    async def sauce(self, ctx):
-       file = open('codes.csv', 'r').read()
-       a = list(file.split(", "))
-       await ctx.send("https://nhentai.net/g/"+ a[random.randint(0, len(a)+1)])
-#additional porn command
-class p(commands.Cog):
-    bots = commands.Bot(command_prefix=['p', 'porn'], help_command=None)
-
-    def __init__(self, bot):
-        self.bot = bot
-
-    @bots.command(name='p')
-    async def p(self, ctx):
-        hyperlink = scraper(url_link = 'https://realbooru.com/index.php?page=post&s=list&tags=brazzers', remove = 5)
-        ctx.send(hyperlink)
-
-
+            #await channel.delete()
+ 
 bots.add_cog(help(bots))
-bots.add_cog(sext(bots))
-bots.add_cog(sextC(bots))
-bots.add_cog(AhegaoPic(bots))
-bots.add_cog(sauce(bots))
-bots.add_cog(p(bots))
+bots.add_cog(text(bots))
+bots.add_cog(tts(bots))
+bots.add_cog(vc(bots))
 bots.run(TOKEN)
